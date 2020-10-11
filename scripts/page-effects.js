c2aca2e902d201ec35e7be0f16d40157aae5dd8b
Grégory <jaulin.gregory@.gmail.com>
2020-10-11 18:13:47 +0200

    Add scroll animation
    Use intersection observer to make div appear from nothing on scroll

--- scripts/page-effects.js
+++ scripts/page-effects.js
@@ -1,17 +1,19 @@
-const header = document.querySelector('header')
-const present = document.querySelector('#presentation')
-const work = document.querySelector('#work')
+// Disable animations on mobile
+if(screen && screen.width > 480) {
+    const header = document.querySelector('header')
+    const present = document.querySelector('#presentation')
+    const work = document.querySelector('#work')
+
+    document.addEventListener('scroll', () => {
+        let scrollBarPosition = window.pageYOffset | document.body.scrollTop
+
+        header.style.setProperty('--bg-position', `${75 - (scrollBarPosition / header.offsetHeight * 25)}%`)
+        present.style.setProperty('--bg-position', `${75 - (scrollBarPosition / present.offsetHeight * 25)}%`)
+        work.style.setProperty('--bg-position', `${75 - (scrollBarPosition / present.offsetHeight * 25)}%`)
+    })
+}
+
 const up_btn = document.querySelector('#up-btn')
-
-document.addEventListener('scroll', () => {
-    if(screen && screen.width < 480) return;
-    
-    let scrollBarPosition = window.pageYOffset | document.body.scrollTop
-
-    header.style.setProperty('--bg-position', `${75 - (scrollBarPosition / header.offsetHeight * 25)}%`)
-    present.style.setProperty('--bg-position', `${75 - (scrollBarPosition / present.offsetHeight * 25)}%`)
-    work.style.setProperty('--bg-position', `${75 - (scrollBarPosition / present.offsetHeight * 25)}%`)
-})
 
 up_btn.addEventListener('click', () => {
     window.scrollTo(0, 0)
---
+++ scripts/scroll-anim.js
@@ -0,0 +1,23 @@
+// Disable animations on mobile
+if(screen && screen.width > 480) {
+    const minRatio = .2
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: minRatio
+    }
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if(entry.intersectionRatio >= minRatio) {
+                entry.target.classList.add("reveal")
+                observer.unobserve(entry.target)
+            }    
+        })
+    }, options)
+
+    // Observe elem with class starting by "anim-"
+    document.querySelectorAll("[class*=anim-]").forEach(elem => {
+        observer.observe(elem)
+    })
+}
